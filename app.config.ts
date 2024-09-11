30a60a379d27db42655aff583e5b246d26aa70e6
inpiniti <inpiniti@gmail.com>
2024-09-11 14:18:10 +0900

    feat: Add navigation links for documentation and products
    This commit adds navigation links for the documentation and products sections in the app. The documentation section includes links to stock, travel, and inpiniti finance pages, providing information and guides related to these topics. The products section includes links to external websites for stock, potatomap, and 중고검색기, allowing users to access these services directly. This enhancement improves the user experience by providing easy access to relevant information and external services.

--- app.config.ts
+++ app.config.ts
@@ -18,7 +18,63 @@ export default defineAppConfig({
         light: "/favicon-96x96.png",
         dark: "/favicon-96x96.png",
       },
-      nav: [],
+      nav: [
+        {
+          title: "문서",
+          links: [
+            {
+              title: "stock",
+              to: "/stock/tensorflow",
+              description:
+                "본 제품은 주식 예측 서비스로 신경망 모델을 구성하고 훈련하여 예측을 하는 서비스 입니다.",
+            },
+            {
+              title: "travel",
+              to: "/travel/introduction",
+              description: "여행 플레너",
+            },
+            {
+              title: "inpiniti finance",
+              to: "/portfolio/portfolio/finace",
+              description: "재무제표 라이브러리 사용법",
+            },
+          ],
+        },
+        {
+          title: "제품",
+          links: [
+            {
+              title: "stock",
+              to: "https://stock.potatomap.com/",
+              description: "주식예측 서비스 입니다.",
+              target: "_blank",
+            },
+            {
+              title: "potatomap",
+              to: "https://potatomap.com/",
+              description: "여행 플레너 입니다.",
+              target: "_blank",
+            },
+            {
+              title: "중고검색기",
+              to: "https://wjd0r.github.io/",
+              description: "중고검색 서비스 입니다.",
+              target: "_blank",
+            },
+          ],
+        },
+        {
+          title: "open source",
+          links: [
+            {
+              title: "inpiniti finance",
+              to: "https://github.com/inpiniti/inpinitiFinance",
+              description: "재무재표 조회 라이브러리 입니다.",
+              target: "_blank",
+            },
+          ],
+        },
+      ],
       links: [
         {
           icon: "lucide:github",
